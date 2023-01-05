9fa262bf2e451305509276409609dde15a97ebdb
Gabriel Lira <gabrieldeio40@gmail.com>
2023-01-05 08:27:07 -0300

    createDepartmentDao implementation

--- src/models/dao/DaoFactory.java
+++ src/models/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package models.dao;
 
+import models.dao.impl.DepartmentDaoJDBC;
 import models.dao.impl.SellerDaoJDBC;
 import models.db.DB;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 
 }
