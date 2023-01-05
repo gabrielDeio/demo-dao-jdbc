acfa6e7ed8de50fe92cf58f43e948f8eed95c476
Gabriel Lira <gabrieldeio40@gmail.com>
2023-01-05 07:45:17 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,11 +25,17 @@ public class Program {
         System.out.println("\n=== TEST 3: seller findAll =====");
         List<Seller> list2 = sellerDao.findAll();
         System.out.println(list2);
-        */
+
         System.out.println("\n=== TEST 4: seller insert =====");
         Seller seller2 = new Seller(null, "Gabril Lira", "gabrieldeio@gmail.com", new Date(), 3500.00, department);
         System.out.println(seller2);
         sellerDao.insert(seller2);
         System.out.println("Inserted! New id = " + seller2.getId());
+        */
+        System.out.println("\n=== TEST 5: seller update =====");
+        Seller seller3 = sellerDao.findById(8);
+        seller3.setName("Gabriel Deió");
+        sellerDao.update(seller3);
+        System.out.println("Update completed");
     }
 }
